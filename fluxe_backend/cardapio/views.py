842b1d323ec912bbf7d1d4859574c5377a59e9e6
Bruno Santos <bruno.ts013@hotmail.com>
2025-11-03 12:53:32 -0300

    ultimas alterações django

--- fluxe_backend/cardapio/views.py
+++ fluxe_backend/cardapio/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import CategoriaProduto
+from .serializers import CategoriaProdutoSerializer
 
-# Create your views here.
+class CategoriaProdutoViewSet(viewsets.ModelViewSet):
+    queryset = CategoriaProduto.objects.all().order_by('ordem_exibicao')
+    serializer_class = CategoriaProdutoSerializer
--- fluxe_backend/config/urls.py
+++ fluxe_backend/config/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from cardapio.views import CategoriaProdutoViewSet # Importa nossa "Visão"
+
+# Cria o router da api
+router = routers.DefaultRouter()
+router.register(r'categorias', CategoriaProdutoViewSet) # Cria a URL 'api/categorias/'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)), # Liga o roteador na URL 'api/'
 ]
