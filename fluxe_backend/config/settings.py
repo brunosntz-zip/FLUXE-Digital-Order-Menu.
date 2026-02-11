b1897d1e7cfbe0bc94d2c29a7b0f79d7e4886279
Bruno Santos <bruno.ts013@hotmail.com>
2026-02-11 20:23:02 -0300

    pelo amor funciona vercel

--- fluxe_backend/config/settings.py
+++ fluxe_backend/config/settings.py
@@ -108,6 +108,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
--- fluxe_backend/config/wsgi.py
+++ fluxe_backend/config/wsgi.py
@@ -1,17 +1,14 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+from pathlib import Path
 from django.core.wsgi import get_wsgi_application
+
+# Adiciona a pasta 'fluxe_backend' ao caminho do Python
+# Isso permite que ele encontre 'config' e 'cardapio'
+current_path = Path(__file__).resolve().parent.parent
+sys.path.append(str(current_path))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
-
 app = application
